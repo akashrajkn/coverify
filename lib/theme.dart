9feb9e73fead8c018d5c36dabfec229a0f9fe7dc
akashrajkn <akashrajkn@gmail.com>
2021-05-03 13:17:17 +0200

    v0.0.1 - empty screen

---
+++ lib/theme.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+const Color         primaryColor         = Color(0xff393E41);
+const MaterialColor primaryColorMaterial = const MaterialColor(
+  0xff393E41,
+  const <int, Color>{
+    50 : const Color(0xff687278),
+    100: const Color(0xff5f686d),
+    200: const Color(0xff555d62),
+    300: const Color(0xff4c5357),
+    400: const Color(0xff42494c),
+    500: const Color(0xff393E41),
+    600: const Color(0xff2f3437),
+    700: const Color(0xff26292c),
+    800: const Color(0xff1c1f21),
+    900: const Color(0xff131516),
+  }
+);
+
+
